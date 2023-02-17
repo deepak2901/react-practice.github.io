3d6ee8541ee1aaa3e935f7c7281b60fb6fdc659b
deepak <deepak.kondapalli29@gmail.com>
2023-02-17 16:07:46 +0530

    setup git pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import "./App.css";
 import {Greet} from './components/Greet'
 import Welcome from "./components/Welcome";
-
 class App extends Component {
   render() {
     return (
