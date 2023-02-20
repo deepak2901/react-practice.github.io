3815d40bf7d91af3e236a39ae19edb3976cf72ac
deepak <deepak.kondapalli29@gmail.com>
2023-02-20 17:19:42 +0530

    final

--- my-app/src/components/Greet.js
+++ my-app/src/components/Greet.js
@@ -4,6 +4,6 @@ import React from 'react'
 //     return <h1>Hello Deepak..</h1>
 // }
 
-export const Greet = () => <h1>Hello Deepak.!!!</h1>
+export const Greet = () => <h1>Hello World.!!!</h1>
 
 export default Greet
